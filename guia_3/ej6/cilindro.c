3b2e25b64dd75cf3caad5f90a939642787bcde45
Santiago Lopez <santiago.lopez.497@gmail.com>
2018-04-23 17:04:30 -0300

    pequenias mejoras

--- guia_3/ej6/cilindro.c
+++ guia_3/ej6/cilindro.c
@@ -1,10 +1,12 @@
-#include <math.h>
+#include "cilindro.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 double cilindro_diametro (double radio, double altura) {
  return radio * 2;
 }
 
-double cilindro_circunferencia (double radio, double, altura) {
+double cilindro_circunferencia (double radio, double altura) {
  return M_PI * radio * 2;
 }
 
@@ -14,4 +16,21 @@ double cilindro_base (double radio, double altura) {
 
 double cilindro_volumen (double radio, double altura) {
  return M_PI * radio * radio * altura;
+}
+
+int main (void)
+{
+ double radio, alt;
+ int c;
+ puts (MSJ_INGRESO);
+ if (scanf ("%lf %lf", &radio, &alt) != 2)
+  return EXIT_FAILURE;
+ while ((c = getchar ()) != EOF && c != '\n')
+  ;
+ printf ("Diametro: %f\n", cilindro_diametro (radio, alt));
+ printf ("Circunferencia: %f\n", cilindro_circunferencia (radio, alt));
+ printf ("Base: %f\n", cilindro_base (radio, alt));
+ printf ("Volumen: %f\n", cilindro_volumen (radio, alt));
+ 
+ return EXIT_SUCCESS;
 }
---
+++ guia_3/ej6/cilindro.h
@@ -0,0 +1,12 @@
+#ifndef CILINDRO__H
+#define CILINDRO__H
+
+#define MSJ_INGRESO "Ingrese radio y altura de un cilindro"
+#define M_PI 3.14159
+
+double cilindro_diametro (double radio, double altura);
+double cilindro_circunferencia (double radio, double altura);
+double cilindro_base (double radio, double altura);
+double cilindro_volumen (double radio, double altura);
+
+#endif
