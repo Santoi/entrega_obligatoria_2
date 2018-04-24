da50797bc1743529417752bfe06b99be87969d68
Santiago Lopez <santiago.lopez.497@gmail.com>
2018-04-24 16:05:12 -0300

    Se arreglo el funcionamiento del ejercicio de busqueda binaria

--- guia_4/ej14/busqueda_binaria.c
+++ guia_4/ej14/busqueda_binaria.c
@@ -1,62 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define NO_ESTA -1
+#define INIC_VEC 0
+#define FIN_VEC 5
+#define MSJ_POS_ELEM "El elemento esta en la posicion"
+#define MSJ_ELEM_INEX "El elemento no se encuentra en el vector"
+#define MSJ_INGRESO "Ingrese un numero a buscar"
 
-typedef enum {
- TRUE,
- FALSE
-} bool_t;
-
-bool_t busqueda_binaria (const int vec [], size_t len, int elem) {
- int vec_izq [5], vec_der [5];
- size_t i;
- printf ("%d\n", vec [len / 2]);
- if (vec [len / 2] == elem)
-  return TRUE;
- if (len == 2) {
-  if (vec [0] == elem)
-   return TRUE;
-  else
-   return FALSE;
- }
- if (len == 1)
-  return FALSE;
-  
- for (i = 0; i < len / 2; i++)
-  vec_izq [i] = vec [i];
- i++;
- while (i < len) {
-  vec_der [i] = vec [i];
-  i++;
- }
+int busqueda_binaria (const int vec [], int elem, size_t izq, size_t der) {
+ int medio = (izq + der) / 2;
  
- if (vec [len / 2] < elem) {
-  len /= 2;
-  len -= 1;
-  busqueda_binaria (vec_der, len, elem);
- }
- else if (len % 2) {
-  len /= 2;
-  len -= 1;
-  busqueda_binaria (vec_izq, len, elem);
- }
- else {
-  len /= 2;
-  busqueda_binaria (vec_izq, len, elem);
- }
+ if (der < izq)
+  return NO_ESTA;
+ 
+ if (elem < vec [medio])
+  der = medio - 1;
+ else if (elem > vec [medio])
+  izq = medio + 1;
+ else
+  return medio;
+ 
+ return busqueda_binaria (vec, elem, izq, der);
 }
+  
 
 int main (void)
 {
  int vector [] = {1, 3, 5, 7, 8, 9};
- size_t len = 6;
- int elem;
+ int elem, pos;
+ 
+ puts (MSJ_INGRESO);
  
  if (scanf ("%d", &elem) != 1)
-  return 1;
+  return EXIT_FAILURE; 
  
- if (busqueda_binaria (vector, len, elem) == TRUE)
-  puts ("true");
+ if ((pos = busqueda_binaria (vector, elem, INIC_VEC, FIN_VEC)) < 0)
+  printf ("%s\n", MSJ_ELEM_INEX);
  else
-  puts ("false");
-  
- return 0;
+  printf("%s %d\n", MSJ_POS_ELEM, pos);
+   
+ return EXIT_SUCCESS;
 }
